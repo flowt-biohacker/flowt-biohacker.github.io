a10da866e92fc84bd2166ad8bbdc21ef3bba6331
martianmartin <flowt-biohacker@gmail.com>
2016-11-25 14:06:16 +0700

    Nikola auto commit.
    Nikola version: 7.8.1

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "deltaFreq"  # (translatable)
+BLOG_AUTHOR = "Martin A. Molina"  # (translatable)
 BLOG_TITLE = "flowt_biohacker"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
